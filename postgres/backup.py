0b7e776ac6c1eac916d421afd4dfccd41072beef
Cameron Hyde <pluckthechicken@hotmail.co.uk>
2021-02-04 13:17:57 +1000

    Scheduled weekly/monthly backups. Improved logging output.

--- postgres/backup.py
+++ postgres/backup.py
@@ -2,8 +2,22 @@
 
 """Make backups of all DBs Postgresql cluster and dispatch to AWS S3."""
 
+from datetime import date
 from backup import daily, weekly, monthly
+from backup.config import WEEKLY_BACKUP_WEEKDAY, MONTHLY_BACKUP_DAY
 
-monthly.make()
-weekly.make()
+today = date.today()
+
+if today.day == MONTHLY_BACKUP_DAY:
+    print('Cascading monthly backups...\n')
+    monthly.make()
+    print()
+
+if today.weekday() == WEEKLY_BACKUP_WEEKDAY:
+    print('Cascading weekly backups...\n')
+    weekly.make()
+    print()
+
+print('Cascading daily backups...\n')
 daily.make()
+print()
--- postgres/backup/config.py
+++ postgres/backup/config.py
@@ -1,14 +1,20 @@
 """Postgres backup configuration."""
 
-# Path to backup location on S3 storage
+# AWS S3 path for backup location
 S3_PATH = 's3://neoform-backup/postgresql/'
-
-# Prefixes to identify backup files by level
-DAILY_PREFIX = 'daily'
-WEEKLY_PREFIX = 'weekly'
-MONTHLY_PREFIX = 'monthly'
 
 # Number of backups at each level
 DAYS = 7
 WEEKS = 4
 MONTHS = 6
+
+# Day of week for weekly backup (0-6 : Mon-Sun)
+WEEKLY_BACKUP_WEEKDAY = 5
+
+# Day of month for monthly backup ( =< 28)
+MONTHLY_BACKUP_DAY = 1
+
+# Prefixes to identify backup files by level
+DAILY_PREFIX = 'daily'
+WEEKLY_PREFIX = 'weekly'
+MONTHLY_PREFIX = 'monthly'
