a02790d15f97a458e5b6d8c2e808eb8fe3d6b07e
Cameron Hyde <pluckthechicken@hotmail.co.uk>
2021-02-04 14:44:40 +1000

    Updating mail config

--- postgres/notifications.py
+++ postgres/notifications.py
@@ -1,18 +1,18 @@
 """Send email to admin."""
 
-import ssl
+import dotenv
 import smtplib
 
-port = 587
-password = None
-smtp_server = "localhost"
-sender_email = "dev@neoformit.com"
-recipient_email = "chyde@neoformit.com"
+port = 25
+password =
+smtp_server =
+sender_email =
+recipient_email =
 
 
 def send_mail(message):
     """Dispatch mail with given content and recipient(s)."""
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    # context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
         server.login(sender_email, password)
         server.sendmail(sender_email, recipient_email, message)
